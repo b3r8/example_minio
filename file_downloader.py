dfba807ee2358c6b33b440c792923b8a98398c13
Bernardo <87157130+b3r8@users.noreply.github.com>
2021-08-10 20:01:47 -0700

    Create file_downloader.py

---
+++ file_downloader.py
@@ -0,0 +1,33 @@
+from minio import Minio
+from minio.error import S3Error
+
+def main():
+    # Create a client with the MinIO server playground, its access key
+    # and secret key.
+    client = Minio(
+        "play.min.io",
+        access_key="Q3AM3UQ867SPQQA43P2F",
+        secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+    )
+
+    # Search for 'myBucket' bucket.
+    found = client.bucket_exists(myBucket)
+    if not found:
+        client.make_bucket("myBucket")
+    else:
+        print("Bucket 'myBucket' already exists")
+    
+    # Download object 'uploaded_requs.txt' from bucket
+    # 'myBucket' as file 'downloaded_requs.txt'.
+    client.fget_object(
+        "myBucket", "uploaded_requs.txt", "downloaded_requs.txt",
+    )
+    print(
+        "object 'uploaded_requs.txt' was successfully downloaded as file 'downloaded_requs.txt' from bucket 'myBucket'"
+    )
+
+if __name__ == "__main__":
+    try:
+        main()
+    except S3Error as exc:
+        print("error occurred", exc)
